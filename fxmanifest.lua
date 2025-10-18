a9f09e0274b0216e1c46f183d3ffeb4a3a71c818
RexShack <rexshackuk@gmail.com>
2025-10-18 21:27:36 +0100

    version checker

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 description 'rex-ranch'
-version '2.0.0'
+version '0.0.1'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -18,7 +18,8 @@ client_scripts {
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/server.lua'
+    'server/server.lua',
+    'versionchecker.lua'
 }
 
 dependencies {
